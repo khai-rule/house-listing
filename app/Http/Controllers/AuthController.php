645e37c7736c6dc533f77ed6269fa7c69513d0f5
Khairul <Qai-rule@hotmail.com>
2023-04-03 06:47:46 +0800

    feat: added basic auth in controller

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
 {
@@ -12,7 +15,22 @@ class AuthController extends Controller
     }
 
     // WHen the form is submitted - create session if valid
-    public function store() {}
+    public function store(Request $request) {
+        
+        if (Auth::attempt($request->validate([
+            "email" => "required|string|email",
+            "password" => "required|string"
+        ]), true /* "remember me" */)) {
+            throw ValidationException::withMessages([
+                "email" => "Authentication failed"
+            ]);
+        }
+
+        // regenerate to avoid attackers using the same ID to send to victims which may get their data
+        $request->session()->regenerate();
+
+        return redirect()->intended();
+    }
 
     // Sign out
     public function destroy() {}
--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { useForm } from '@inertiajs/vue3';
+import { useForm } from "@inertiajs/vue3";
 
 const form = useForm({
     email: null,
@@ -14,13 +14,23 @@ const login = () => form.post(route("login.store"));
         <div class="w-1/2 mx-auto">
             <div class="py-2">
                 <label for="email" class="label">E-Mail (username)</label>
-                <input id="email" type="text" class="input" />
+                <input
+                    id="email"
+                    type="text"
+                    class="input"
+                    v-model="form.email"
+                />
                 <div class="input-error">Potential Erros</div>
             </div>
 
             <div class="py-2">
                 <label for="password" class="label">Password (username)</label>
-                <input id="password" type="password" class="input" />
+                <input
+                    id="password"
+                    type="password"
+                    class="input"
+                    v-model="form.password"
+                />
                 <div class="input-error">Potential Erros</div>
             </div>
 

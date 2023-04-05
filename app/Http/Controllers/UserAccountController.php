86a40264a03049e4c338383798b4a440d14820b2
Khairul <Qai-rule@hotmail.com>
2023-04-06 06:18:50 +0800

    refactor: added password hashing globally

--- app/Http/Controllers/UserAccountController.php
+++ app/Http/Controllers/UserAccountController.php
@@ -22,8 +22,8 @@ class UserAccountController extends Controller
             "email" => "required|email|unique:users",
             "password" => "required|min:8|confirmed", // When using "confirmed", Laravel will expect another field called password_confirmation 
         ]));
-        $user->password = Hash::make($user->password); // Hashing with bcrypt by default
-        $user->save(); // Save to database
+        //* $user->password = Hash::make($user->password); // Hashing with bcrypt by default --> don't have to write this if you have hash password globally (see User.php)
+        //* $user->save(); // Save to database // don't need this if you hash globally. Change User::make to User::create
         Auth::login($user); // This would log them in after account is created
 
         return redirect()->route("listing.index")

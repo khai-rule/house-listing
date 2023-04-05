e48ee43cd927d72f3c0445decb995b5582b839cd
Khairul <Qai-rule@hotmail.com>
2023-04-06 06:42:01 +0800

    feat: link login to create page and vice versa

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -1,15 +1,3 @@
-<script setup>
-import { useForm } from "@inertiajs/vue3";
-
-// The useForm is automatically connected to the Auth Controller
-const form = useForm({
-    email: null,
-    password: null,
-});
-
-const login = () => form.post(route("login.store"));
-</script>
-
 <template>
     <form @submit.prevent="login">
         <div class="w-1/2 mx-auto">
@@ -21,7 +9,9 @@ const login = () => form.post(route("login.store"));
                     class="input"
                     v-model="form.email"
                 />
-                <div v-if="form.errors.email" class="input-error">{{ form.errors.email }}</div>
+                <div v-if="form.errors.email" class="input-error">
+                    {{ form.errors.email }}
+                </div>
             </div>
 
             <div class="py-2">
@@ -32,12 +22,37 @@ const login = () => form.post(route("login.store"));
                     class="input"
                     v-model="form.password"
                 />
-                <div v-if="form.errors.password" class="input-error">{{ form.errors.password }}</div>
+                <div v-if="form.errors.password" class="input-error">
+                    {{ form.errors.password }}
+                </div>
             </div>
 
             <div class="py-4">
                 <button class="btn-primary w-full" type="submit">Login</button>
+
+                <div class="mt-4 text-center">
+                    <div class="text-sm">
+                        Need an account?
+                        <Link
+                            :href="route('user-account.create')"
+                            class="text-gray-500"
+                            >Click here
+                        </Link>
+                    </div>
+                </div>
             </div>
         </div>
     </form>
 </template>
+
+<script setup>
+import { useForm, Link } from "@inertiajs/vue3";
+
+// The useForm is automatically connected to the Auth Controller
+const form = useForm({
+    email: null,
+    password: null,
+});
+
+const login = () => form.post(route("login.store"));
+</script>
--- resources/js/Pages/UserAccount/Create.vue
+++ resources/js/Pages/UserAccount/Create.vue
@@ -55,13 +55,24 @@
                 <button class="btn-primary w-full" type="submit">
                     Create Account
                 </button>
+
+                <div class="mt-4 text-center">
+                    <div class="text-sm">
+                        Already have an account?
+                        <Link
+                            :href="route('login')"
+                            class="text-gray-500"
+                            >Click here
+                        </Link>
+                    </div>
+                </div>
             </div>
         </div>
     </form>
 </template>
 
 <script setup>
-import { useForm } from "@inertiajs/vue3";
+import { useForm, Link } from "@inertiajs/vue3";
 
 // The useForm is automatically connected to the Auth Controller
 const form = useForm({

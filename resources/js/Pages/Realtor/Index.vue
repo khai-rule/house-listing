58b14fb0c9db565f758abe81cdafb2214f065474
Khairul <Qai-rule@hotmail.com>
2023-04-15 06:25:45 +0800

    added RealtorListing controller and routes

---
+++ resources/js/Pages/Realtor/Index.vue
@@ -0,0 +1,7 @@
+<template>
+Say gekko
+</template>
+
+<script setup>
+
+</script>
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\IndexController;
 use App\Http\Controllers\ListingController;
+use App\Http\Controllers\RealtorListingController;
 use App\Http\Controllers\UserAccountController;
 
 /*
@@ -39,6 +40,13 @@ Route::delete("logout", [AuthController::class, "destroy"])
 
 Route::resource("user-account", UserAccountController::class)
     ->only(["create", "store"]);
+
+Route::prefix("realtor")
+    ->name("realtor.")
+    ->middleware("auth")
+    ->group(function() {
+        Route::resource("listing", RealtorListingController::class);
+    });
 /*
 * This is automatically generated from the controller
 /login/create
